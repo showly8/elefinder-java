051b4e6dd3e8d2a2077f03c195f37b045e1b07cd
showly <showly@qq.com>
2020-05-17 15:18:17 +0800

    create demo

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * Hello world!
  *
@@ -9,5 +14,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        WebDriver webDriver=new SafariDriver();
+        String url = "https://www.baidu.com";
+        webDriver.get(url);
+        webDriver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
+        System.out.println("title:"+webDriver.getTitle());
     }
 }
